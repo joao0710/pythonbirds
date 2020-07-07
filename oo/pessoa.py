0cda1b2ced4bb8e53ded37b2c50384d7f63500d8
joaovictor <joao0710@hotmail.com>
2020-07-06 23:05:32 -0300

    Criado atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -10,6 +10,16 @@ class Pessoa:
     def cumprimentar(self):
         return 'Olá'
 
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
+
+
 if __name__ == '__main__':
     renzo = Pessoa(nome='Renzo')
     luciano = Pessoa(renzo, nome='Luciano')
@@ -30,3 +40,7 @@ if __name__ == '__main__':
     luciano.olhos = 1
     print(luciano.__dict__)
     print(renzo.__dict__)
+
+    print(Pessoa.metodo_estatico(), luciano.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), luciano.nome_e_atributos_de_classe())
+
