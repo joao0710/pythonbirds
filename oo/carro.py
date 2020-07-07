e23a07cdd2af79de77270753a1fdd07cf010cbcd
joaovictor <joao0710@hotmail.com>
2020-07-07 02:07:27 -0300

    Implemetação do objeto carro utilizando as classes Motor e Direcao como parte de sua composição

--- oo/carro.py
+++ oo/carro.py
@@ -64,6 +64,22 @@ os seguintes atributos:
     >>> direcao.girar_a_esquerda()
     >>> direcao.valor
     'Norte'
+    >>> carro = Carro(direcao, motor)
+    >>> carro.calcular_velocidade()
+    0
+    >>> carro.acelerar()
+    >>> carro.calcular_velocidade()
+    1
+    >>> carro.acelerar()
+    >>> carro.calcular_velocidade()
+    2
+    >>> carro.frear()
+    >>> carro.calcular_velocidade()
+    0
+    >>> carro.calcular_direcao()
+    'Norte'
+
+
 
 """
 NORTE = 'Norte'
@@ -100,3 +116,27 @@ class Motor:
     def frear(self):
         self.velocidade -= 2
         self.velocidade = max(0, self.velocidade)
+
+
+class Carro:
+    def __init__(self, direcao, motor):
+        self.direcao = direcao
+        self.motor = motor
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        self.motor.acelerar()
+
+    def frear(self):
+        self.motor.frear()
+
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        self.direcao.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        self.direcao.girar_a_esquerda()
